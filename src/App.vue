fee31befe37d0ef7e8b7a33ae4c46d863a705300
Taoweidong <taowd@outlook.com>
2023-01-01 22:21:48 +0800

    更新文档格式

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="app" :class="style ? 'theme1' : ''">
-    <router-view></router-view>
-  </div>
+	<div id="app" :class="style ? 'theme1' : ''">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-import bus from '@/utils/bus';
+	import bus from '@/utils/bus';
 
-export default {
-  data(){
-    return{
-      style: false
-    }
-  },
-  created(){
-    bus.$on('global_theme', (msg) => {    // 接收子组件传来 global_theme 的值
-      this.style = msg;
-    });
-  }
-}
+	export default {
+		data() {
+			return {
+				style: false
+			}
+		},
+		created() {
+			bus.$on('global_theme', (msg) => { // 接收子组件传来 global_theme 的值
+				this.style = msg;
+			});
+		}
+	}
 </script>
